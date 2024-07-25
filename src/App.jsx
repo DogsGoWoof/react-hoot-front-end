2a8dc494a6dc3632a63f32d9b588c66ef952fbbc
jason <jason_tan75@hotmail.com>
2024-07-25 15:31:09 -0400

    lecture up to creating comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,57 @@
-import { useState, createContext } from 'react';
-import { Routes, Route } from 'react-router-dom';
+
+import { useState, createContext, useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+
+//___Components___//
 import NavBar from './components/NavBar/NavBar';
 import Landing from './components/Landing/Landing';
 import Dashboard from './components/Dashboard/Dashboard';
 import SignupForm from './components/SignupForm/SignupForm';
 import SigninForm from './components/SigninForm/SigninForm';
-import * as authService from '../src/services/authService'; // import the authservice
+import HootList from './components/HootList/HootList';
+import HootDetails from './components/HootDetails/HootDetails';
+import HootForm from './components/HootForm/HootForm';
 
+//___Services___//
+import * as authService from '../src/services/authService'; // import the authservice
+import * as hootService from './services/hootService';
+
+//___Context___//
 export const AuthedUserContext = createContext(null);
 
 const App = () => {
+
+  //___States___//
   const [user, setUser] = useState(authService.getUser()); // using the method from authservice
+  const [hoots, setHoots] = useState([]);
 
   const handleSignout = () => {
     authService.signout();
     setUser(null);
   };
+
+  const navigate = useNavigate();
+
+  // const handleAddHoot = async (hootFormData) => {
+  //   console.log('hootFormData', hootFormData);
+  //   navigate('/hoots');
+  // };
+  const handleAddHoot = async (hootFormData) => {
+    const newHoot = await hootService.create(hootFormData);
+    setHoots([newHoot, ...hoots]);
+      // add new value to front of the array to display newest entry at the top
+    navigate('/hoots');
+  };
+
+  useEffect(() => {
+    const fetchAllHoots = async () => {
+      const hootsData = await hootService.index();
+      // console.log('hootsData:', hootsData);
+      // Set state:
+      setHoots(hootsData);
+    };
+    if (user) fetchAllHoots();
+  }, [user]);
 
   return (
     <>
@@ -23,8 +59,20 @@ const App = () => {
         <NavBar user={user} handleSignout={handleSignout} />
         <Routes>
           {user ? (
-            <Route path="/" element={<Dashboard user={user} />} />
+            // Protected Routes:
+            <>
+              <Route path="/" element={<Dashboard user={user} />} />
+              <Route path="/hoots" element={<HootList hoots={hoots} />} />
+              <Route path="/hoots/:hootId" element={<HootDetails />} />
+              {/* <Route
+                path="/hoots/new"
+                element={<h1>New Hoot</h1>}
+              /> */}
+              {/* <Route path="/hoots/new" element={<HootForm />} /> */}
+              <Route path="/hoots/new" element={<HootForm handleAddHoot={handleAddHoot} />} />
+            </>
           ) : (
+            // Public Route:
             <Route path="/" element={<Landing />} />
           )}
           <Route path="/signup" element={<SignupForm setUser={setUser} />} />
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,13 +9,25 @@ const NavBar = ({ handleSignout }) => {
       {user ? (
         <nav>
           <ul>
-            <li>Welcome, {user.username}</li>
             <li>
-              <Link to="/">Dashboard</Link>
+              <Link to='/'>
+                HOME
+              </Link>
             </li>
             <li>
-              <Link to="" onClick={handleSignout}>
-                Sign Out
+              <Link to='/hoots'>
+                HOOTS
+              </Link>
+            </li>
+            <li>
+              <Link to="/hoots/new">NEW HOOT</Link>
+            </li>
+            {/* <li>
+              <Link to="/">Dashboard</Link>
+            </li> */}
+            <li>
+              <Link to='' onClick={handleSignout}>
+                SIGN OUT
               </Link>
             </li>
           </ul>
